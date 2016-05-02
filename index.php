8492b735ea54dbf0df976bc1decb2f611f4180f6
CranCraft <felix_ohlsen@yahoo.de>
2016-05-02 16:40:51 +0200

    Erstellung

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>leftover</title>
+
+		<!-- Bootstrap -->
+		<link href="main.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="bootstrap-3.3.6/js/bootstrap.min.js"></script>
+		<!-- Main JavaScript Datei einbinden-->
+		<script src="js/main.js" type="text/javascript"></script>
+		
+	</head>
+	<body>
+		<div id="wrapper">
+			<div class="container-fluid">
+				<div id="header">
+					<div id="bottomHeaderContent">
+						<div id="headerLogo">
+							<div class="row">
+								<div class="col-xs-12">
+									<img src="img/logoBanner.png" class="img-responsive">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="main">
+					<div class="row">
+						<a href="#">
+						<div class="categoryBlock col-md-3 col-md-offset-2 col-xs-12">
+							<img src="img/nudeln_frei.png" class="img-responsive">
+							<div class="btn btn-default btn-block" role="button">
+								nudeln
+							</div>
+						</div> </a>
+						<a href="#">
+						<div class="categoryBlock col-md-3 col-md-offset-2 col-xs-12">
+							<img src="img/nudeln_frei.png" class="img-responsive">
+							<div class="btn btn-default btn-block" role="button">
+								nudeln
+							</div>
+						</div> </a>
+					</div>
+					<div class="row">
+						<a href="#">
+						<div class="categoryBlock col-md-3 col-md-offset-2 col-xs-12">
+							<img src="img/nudeln_frei.png" class="img-responsive">
+							<div class="btn btn-default btn-block" role="button">
+								nudeln
+							</div>
+						</div> </a>
+						<a href="#">
+						<div class="categoryBlock col-md-3 col-md-offset-2 col-xs-12">
+							<img src="img/nudeln_frei.png" class="img-responsive">
+							<div class="btn btn-default btn-block" role="button">
+								nudeln
+							</div>
+						</div> </a>
+					</div>
+				</div>
+				<div id="footer"></div>
+			</div>
+		</div>
+		  <nav class="navbar navbar-fixed-right navbar-minimal animate" role="navigation">
+		<div class="navbar-toggler animate">
+			<span class="menu-icon"></span>
+		</div>
+		<ul class="navbar-menu animate">
+			<li>
+				<a href="#about-us" class="animate">
+					<span class="desc animate"> Who We Are </span>
+					<span class="glyphicon glyphicon-user"></span>
+				</a>
+			</li>
+			<li>
+				<a href="#blog" class="animate">
+					<span class="desc animate"> What We Say </span>
+					<span class="glyphicon glyphicon-info-sign"></span>
+				</a>
+			</li>
+			<li>
+				<a href="#contact-us" class="animate">
+					<span class="desc animate"> How To Reach Us </span>
+					<span class="glyphicon glyphicon-comment"></span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+	</body>
+</html>
