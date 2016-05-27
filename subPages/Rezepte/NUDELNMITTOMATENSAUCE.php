4810f016139b08734c41c39d6ea20d9202a0277c
CranCraft <felix_ohlsen@yahoo.de>
2016-05-27 10:02:48 +0200

    Rezeptseite hinzugefügt

---
+++ subPages/Rezepte/NUDELNMITTOMATENSAUCE.php
@@ -0,0 +1,79 @@
+<!-- Header Datei wird eingefügt-->
+
+<?php
+include ('../../header.php');
+?>
+<body>
+	<div id="wrapper">
+		<div class="container-fluid">
+			<div id="header">
+				<div id="bottomHeaderContent">
+					<div id="headerLogo">
+						<div class="row">
+							<div class="col-xs-12">
+								<a href="../../index.php"> <img src="../../img/logoBanner.png" class="img-responsive"> </a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="main" class="rezept">
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4 col-xs-12 siteTitle">
+						Nudel Rezepte
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 col-xs-12">
+						<img src="../../img/nudeln_frei.png" class="img-responsive">
+					</div>
+				</div>
+				<div class="row">
+					<h4>Zutaten</h4>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+							<p class="text-center">
+								1 Zwiebel
+								<br/>1 Knoblauchzehe
+								<br/>etwas Olivenöl
+								<br/>1 große Dose Tomaten (800g) oder die gleiche Menge passierte Tomaten
+								<br/>Salz, Pfeffer
+								<br/>Basilikum, (evtl. auch noch Thymian + Rosmarin+Oregano)
+								<br/>Parmesan
+								<br/> 
+							</p>
+						</div>
+						</div>
+				<div class="row">
+					<h4>Zubereitung</h4>
+				</div>
+						<div class="row">
+						<div class="col-xs-12">
+						<p class="text-center">
+							Zwiebel und Knoblauch schälen und würfeln. etwas Öl in einer großen Pfanne erhitzen. Zwiebel und Knoblauch darin glasig dünsten.<br/><br/>
+
+							Tomaten samt Saft zugeben. Aufkochen und auf kleiner Flamm 30 Min. köcheln lassen. Evtl.  pürieren - bei passierten Tomaten braucht man das nicht. Zum Schluss noch etwa 2 El Öl unterziehen und mit Salz, Pfeffer gehackten Kräutern würzen. Mit Nudeln und frisch geriebenem Parmesan servieren.<br/><br/>
+
+							Die Kräuter e ich in der Sauce ganz besonders wichtig! Am besten natürlich frisch, TK-Kräuter verwende ich aber auch ab und an. Nur die Getrockneten mag ich nicht so - sie schmecken einfach nicht intensiv genug. Ihr könnt Euch auf Basilikum beschränken, Thymian, Rosmarin und Oregano passen aber auch noch super dazu. Je mehr Kräuter, umso besser :)!<br />
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="footer" class="container-fluid">
+			<!-- Footer Datei wird eingefügt-->
+
+			<?php
+			include ('../../footer.php');
+			?>
+		</div>
+	</div>
+	<!-- Menu Datei wird eingefügt
+
+	<?php
+	include ('subPages/menu.php');
+	?>
+	-->
+</body>
+</html>
