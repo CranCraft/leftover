5cd28c2f91921633cc4293774f40b0e41213dff3
Freigeist <admin@white-it.net>
2016-06-10 10:55:47 +0200

    Dynamic footer paths

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
+<!-- $path was defined in header.php, which is also included in every page -->
 		<div class="row">
 				<div class="col-xs-12">
 					<ul class="footerList firstList">
 						<li>
-							<a href="/leftover/leftover/subPages/kategorieNudeln.php">Nudelrezepte</a>
+							<a href="<?php echo $path; ?>/subPages/kategorieNudeln.php">Nudelrezepte</a>
 						</li>
 						<li>
-							<a href="/leftover/leftover/subPages/kategorieReis.php">Reisrezepte</a>
+							<a href="<?php echo $path; ?>/subPages/kategorieReis.php">Reisrezepte</a>
 						</li>
 						<li>
-							<a href="/leftover/leftover/subPages/kategorieBrot.php">Brotrezepte</a>
+							<a href="<?php echo $path; ?>/subPages/kategorieBrot.php">Brotrezepte</a>
 						</li>
 						<li>
-							<a href="/leftover/leftover/subPages/kategorieNachtisch.php">Nachtischrezepte</a>
+							<a href="<?php echo $path; ?>/subPages/kategorieNachtisch.php">Nachtischrezepte</a>
 						</li>
 					</ul>
 				</div>
@@ -20,10 +21,10 @@
 				<div class="col-xs-12">
 					<ul class="footerList secondList">
 						<li>
-							<a href="/leftover/leftover/subPages/impressum.php">Impressum</a>
+							<a href="<?php echo $path; ?>/subPages/impressum.php">Impressum</a>
 						</li>
 						<li>
-							<a href="/leftover/leftover/subPages/kontakt.php">Kontakt</a>
+							<a href="<?php echo $path; ?>/subPages/kontakt.php">Kontakt</a>
 						</li>
 					</ul>
 				</div>
