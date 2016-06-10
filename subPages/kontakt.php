6c6aeca02ce2e38fa0d60b1db10ca043ba209096
CranCraft <felix_ohlsen@yahoo.de>
2016-06-10 11:06:02 +0200

    Kontakt und Impressum fertig

--- subPages/kontakt.php
+++ subPages/kontakt.php
@@ -19,49 +19,47 @@ include ('../header.php');
 			</div>
 			<div id="main">
 				<div class="row">
-					<div id="contact_form" class="col-xs-10 col-xs-offset-1">
-						<div class="well well-sm">
-							<form class="form-horizontal" action="" method="post">
-								<fieldset>
-									<legend class="text-center">
-										Schreib Uns
-									</legend>
+					<div id="contact_form" class="col-xs-10 col-xs-offset-1 text-center">
+						<form class="form-horizontal" action="" method="post">
+							<fieldset>
+								<legend class="text-center">
+									Schreib Uns
+								</legend>
 
-									<!-- Name input-->
-									<div class="form-group">
-										<label class="col-md-3 control-label" for="name">Name</label>
-										<div class="col-md-9">
-											<input id="name" name="name" type="text" placeholder="Dein Name" class="form-control">
-										</div>
+								<!-- Name input-->
+								<div class="form-group">
+									<label class="control-label" for="name">Name</label>
+									<div class="col-xs-12">
+										<input id="name" name="name" type="text" placeholder="Dein Name" class="form-control">
 									</div>
+								</div>
 
-									<!-- Email input-->
-									<div class="form-group">
-										<label class="col-md-3 control-label" for="email">Deine E-Mail</label>
-										<div class="col-md-9">
-											<input id="email" name="email" type="text" placeholder="Deine E-Mail Adresse" class="form-control">
-										</div>
+								<!-- Email input-->
+								<div class="form-group">
+									<label class="control-label" for="email">Deine E-Mail</label>
+									<div class="col-xs-12">
+										<input id="email" name="email" type="text" placeholder="Deine E-Mail Adresse" class="form-control">
 									</div>
+								</div>
 
-									<!-- Message body -->
-									<div class="form-group">
-										<label class="col-md-3 control-label" for="message">Deine Nachricht</label>
-										<div class="col-md-9">
-											<textarea class="form-control" id="message" name="message" placeholder="Hier kannst du deine Nachricht hinterlassen..." rows="5"></textarea>
-										</div>
+								<!-- Message body -->
+								<div class="form-group">
+									<label class="control-label" for="message">Deine Nachricht</label>
+									<div class="col-xs-12">
+										<textarea class="form-control" id="message" name="message" placeholder="Hier kannst du deine Nachricht hinterlassen..." rows="5"></textarea>
 									</div>
+								</div>
 
-									<!-- Form actions -->
-									<div class="form-group">
-										<div class="col-md-12 text-right">
-											<button id="submit_button" type="submit" class="btn btn-primary btn-lg">
-												Absenden
-											</button>
-										</div>
+								<!-- Form actions -->
+								<div class="form-group">
+									<div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1  text-right">
+										<button type="submit" class="btn-default btn-lg btn-justify">
+											Absenden
+										</button>
 									</div>
-								</fieldset>
-							</form>
-						</div>
+								</div>
+							</fieldset>
+						</form>
 					</div>
 				</div>
 			</div>
